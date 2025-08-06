8ab0ac84b305c14771e885fa13727771484cc367
J0joa <4j0joa@gmail.com>
2025-08-06 14:21:59 -0500

    primer commit

--- frontend/react-app/vite.config.js
+++ frontend/react-app/vite.config.js
@@ -7,5 +7,5 @@ import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base:'https://J0joa.github.io/appChatBot'
+  base:'aplicaciónChatBot',
 })
